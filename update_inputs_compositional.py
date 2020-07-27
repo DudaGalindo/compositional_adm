ed34373a357afc3e59351187ecee69e3bd32e81b
Eduarda <meduardagalindo8@gmail.com>
2020-07-26 23:29:44 -0300

    moved water initial mole numbers and rock initial pore volume calculation to here and includded well pressure before flash calculation (it's not final this last modification, it will be evaluated with benchmarks

--- update_inputs_compositional.py
+++ update_inputs_compositional.py
@@ -22,21 +22,30 @@ if dd['deletar_results']:
             os.remove(os.path.join(results, f))
 
 class FluidProperties:
-    def __init__(self):
+    def __init__(self, wells):
         self.P = np.array([data_loaded['Pressure']['r1']['value']]).astype(float)
         self.T = data_loaded['Temperature']['r1']['value']
         self.component_molar_fractions = np.ones([ctes.n_components, ctes.n_phases, ctes.n_volumes])
         self.phase_molar_densities = np.empty([1, ctes.n_phases, ctes.n_volumes])
-        self.phase_densities = np.empty(self.phase_molar_densities.shape)
+        self.phase_densities = np.empty_like(self.phase_molar_densities)
+        self.component_mole_numbers = np.empty([ctes.n_components, ctes.n_volumes])
+        self.update_initial_porous_volume()
+        self.P = self.P * np.ones(ctes.n_volumes)
+        self.P[wells['ws_p']] = wells['values_p']
         self.L = np.empty(len(self.P))
         self.V = np.empty(len(self.P))
         if ctes.load_k:
             self.z = np.array([data_loaded['compositional_data']['component_data']['z']]).astype(float).T
+            self.z = self.z * np.ones(ctes.n_volumes)
+
         else: z = []
 
+    def update_initial_porous_volume(self):
+        self.Vp = ctes.porosity * ctes.Vbulk * (1 + ctes.Cf*(self.P - ctes.Pf))
+
     def inputs_fluid_properties(self):
-        self.z = self.z * np.ones(ctes.n_volumes)
-        self.P = self.P * np.ones(ctes.n_volumes)
+        #self.z = self.z * np.ones(ctes.n_volumes)
+        #self.P = self.P * np.ones(ctes.n_volumes)
         self.L = self.L * np.ones(ctes.n_volumes).astype(float)
         self.V = self.V * np.ones(ctes.n_volumes).astype(float)
         #coef_vc7 = np.array([21.573, 0.015122, -27.6563, 0.070615])
@@ -44,12 +53,14 @@ class FluidProperties:
                             #coef_vc7[2] * np.mean([ctes.SG[ctes.C7 == 1]]) + coef_vc7[3] \
                             #* np.mean(ctes.Mw[ctes.C7 == 1]) * np.mean([ctes.SG[ctes.C7 == 1]])
 
-    def inputs_water_properties(self):
+    def inputs_water_properties(self, M):
         self.phase_densities[0,ctes.n_phases-1,:] = data_loaded['compositional_data']['water_data']['rho_W']
         self.ksi_W0 = self.phase_densities[0,ctes.n_phases-1,:] / ctes.Mw_w
         self.ksi_W = self.ksi_W0
         self.rho_W = self.ksi_W * ctes.Mw_w
+        self.Sw = M.data['saturation']
         #self.phase_molar_densities[0,ctes.n_phases-1,:] = self.ksi_W0
         self.component_molar_fractions[ctes.n_components-1,ctes.n_phases-1,:] = 1
         self.component_molar_fractions[ctes.n_components-1,0:ctes.n_phases-1,:] = 0
         self.component_molar_fractions[0:ctes.n_components-1,ctes.n_phases-1,:] = 0
+        self.component_mole_numbers[-1,:] = self.Vp * self.ksi_W * self.Sw
