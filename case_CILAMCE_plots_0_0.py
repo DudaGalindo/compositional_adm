cf3706ae8a4059b8619b97ea0e3c648fb3b588b7
DudaGalindo <meduardagalindo8@gmail.com>
2020-08-29 15:04:26 -0300

    L2 and L1 error norms included

--- case_CILAMCE_plots_0_0.py
+++ case_CILAMCE_plots_0_0.py
@@ -27,7 +27,7 @@ for arq in arquivos:
         n=128
         mode = 'LLF'
 
-        datas = np.load('flying/results_water_inj_6k_128_MUSCL_modified_case_LLF_2826.npy', allow_pickle=True)
+        '''datas = np.load('flying/results_water_inj_6k_128_MUSCL_modified_case_LLF_2826.npy', allow_pickle=True)
         for data in datas[2:]:
             SwLLF = data[5]
             SoLLF = data[6]
@@ -85,9 +85,9 @@ for arq in arquivos:
         plt.xlabel('Distance')
         plt.grid()
         plt.savefig('results/compositional/MUSCL_tests/saturation_gas_6k_MUSCL_modified' +'{}'.format(n) +mode+ '.png')
+        '''
 
 
-        import pdb; pdb.set_trace()
         datas = np.load('flying/results_water_inj_6k_8_MUSCL_modified_case_LLF_572.npy', allow_pickle=True)
         for data in datas[2:]:
             Sw_MUSCL8 = data[5]
@@ -96,8 +96,9 @@ for arq in arquivos:
             pressure_MUSCL8 = data[4]/1e3
             n=8
             x8 = np.linspace(0.54624/n,2731.2/n*(n-1),n)
-            e8_max = max(abs(P[0:-1:8]-pressure_MUSCL8))
-            e8_L2 = (sum((P[0:-1:8]-pressure_MUSCL8)**2)*(1/15)**2)**(1/2)
+            e8_max = max(abs(Sw_CMG[0:-1:int(1024/8)]-Sw_MUSCL8))
+            e8_L1 = (sum(abs(Sw_CMG[0:-1:int(1024/8)]-Sw_MUSCL8))*(1/8))
+            e8_L2 = (sum((Sw_CMG[0:-1:int(1024/8)]-Sw_MUSCL8)**2)*(1/8))**(1/2)
 
 
         datas = np.load('flying/results_water_inj_6k_16_MUSCL_modified_case_LLF_1020.npy', allow_pickle=True)
@@ -108,10 +109,12 @@ for arq in arquivos:
             pressure_MUSCL16 = data[4]/1e3
             n=16
             x16 = np.linspace(0.54624/n,2731.2/n*(n-1),n)
-            e16_max = max(abs(P[0:-1:16]-pressure_MUSCL16))
-            e16_L2 = (sum((P[0:-1:16]-pressure_MUSCL16)**2)*(1/15)**2)**(1/2)
-            R16_L2 = math.log(e8_L2/e16_L2,3)
-            R16_max = math.log(e8_max/e16_max, 3)
+            e16_max = max(abs(Sw_CMG[0:-1:int(1024/16)]-Sw_MUSCL16))
+            e16_L1 = (sum(abs(Sw_CMG[0:-1:int(1024/16)]-Sw_MUSCL16))*(1/16))
+            e16_L2 = (sum((Sw_CMG[0:-1:int(1024/16)]-Sw_MUSCL16)**2)*(1/16))**(1/2)
+            R16_L2 = math.log(e8_L2/e16_L2,2)
+            R16_L1 = math.log(e8_L1/e16_L1,2)
+            R16_max = math.log(e8_max/e16_max, 2)
 
         datas = np.load('flying/results_water_inj_6k_32_MUSCL_modified_case_LLF_1693.npy', allow_pickle=True)
         for data in datas[2:]:
@@ -121,10 +124,12 @@ for arq in arquivos:
             pressure_MUSCL32 = data[4]/1e3
             n=32
             x32 = np.linspace(0.54624/n,2731.2/n*(n-1),n)
-            e32_max = max(abs(P[0:-1:32]-pressure_MUSCL32))
-            e32_L2 = (sum((P[0:-1:32]-pressure_MUSCL32)**2)*(1/15)**2)**(1/2)
-            R32_L2 = math.log(e16_L2/e32_L2,3)
-            R32_max = math.log(e16_max/e32_max, 3)
+            e32_max = max(abs(Sw_CMG[0:-1:int(1024/32)]-Sw_MUSCL32))
+            e32_L1 = (sum(abs(Sw_CMG[0:-1:int(1024/32)]-Sw_MUSCL32))*(1/32))
+            e32_L2 = (sum((Sw_CMG[0:-1:int(1024/32)]-Sw_MUSCL32)**2)*(1/32))**(1/2)
+            R32_L2 = math.log(e16_L2/e32_L2,2)
+            R32_L1 = math.log(e16_L1/e32_L1,2)
+            R32_max = math.log(e16_max/e32_max, 2)
 
         datas = np.load('flying/results_water_inj_6k_64_MUSCL_modified_case_LLF_1460.npy', allow_pickle=True)
         for data in datas[2:]:
@@ -134,10 +139,12 @@ for arq in arquivos:
             pressure_MUSCL64 = data[4]/1e3
             n=64
             x64 = np.linspace(0.54624/n,2731.2/n*(n-1),n)
-            e64_max = max(abs(P[0:-1:64]-pressure_MUSCL64))
-            e64_L2 = (sum((P[0:-1:64]-pressure_MUSCL64)**2)*(1/15)**2)**(1/2)
-            R64_L2 = math.log(e16_L2/e64_L2,3)
-            R64_max = math.log(e16_max/e64_max, 3)
+            e64_max = max(abs(Sw_CMG[0:-1:int(1024/64)]-Sw_MUSCL64))
+            e64_L1 = (sum(abs(Sw_CMG[0:-1:int(1024/64)]-Sw_MUSCL64))*(1/64))
+            e64_L2 = (sum((Sw_CMG[0:-1:int(1024/64)]-Sw_MUSCL64)**2)*(1/64))**(1/2)
+            R64_L2 = math.log(e32_L2/e64_L2, 2)
+            R64_L1 = math.log(e32_L1/e64_L1, 2)
+            R64_max = math.log(e32_max/e64_max, 2)
 
         datas = np.load('flying/results_water_inj_6k_128_MUSCL_modified_case_LLF_2826.npy', allow_pickle=True)
         for data in datas[2:]:
@@ -147,11 +154,29 @@ for arq in arquivos:
             pressure_MUSCL128 = data[4]/1e3
             n=128
             x128 = np.linspace(0.54624/n,2731.2/n*(n-1),n)
-            e128_max = max(abs(P[0:-1:128]-pressure_MUSCL128))
-            e128_L2 = (sum((P[0:-1:128]-pressure_MUSCL128)**2)*(1/15)**2)**(1/2)
-            R128_L2 = math.log(e64_L2/e128_L2,3)
-            R128_max = math.log(e64_max/e128_max, 3)
+            e128_max = max(abs(Sw_CMG[0:-1:int(1024/128)]-Sw_MUSCL128))
+            e128_L1 = (sum(abs(Sw_CMG[0:-1:int(1024/128)]-Sw_MUSCL128))*(1/128))
+            e128_L2 = (sum((Sw_CMG[0:-1:int(1024/128)]-Sw_MUSCL128)**2)*(1/128))**(1/2)
+            R128_L2 = math.log(e64_L2/e128_L2,2)
+            R128_L1 = math.log(e64_L1/e128_L1,2)
+            R128_max = math.log(e64_max/e128_max, 2)
 
+        plt.figure(1)
+        x = np.log(np.array([8,16,32,64,128]))
+        y = np.log(np.array([e8_L2, e16_L2, e32_L2, e64_L2, e128_L2]))
+        plt.plot(x, y,'-p')
+        ref_line = x[0:2]/2
+
+        plt.plot(x[0:2], -np.array([ref_line[1],ref_line[1]])-2.7,'-k')
+        plt.plot(np.array([x[0],x[0]]),-ref_line-2.7,'-k')
+        plt.plot(x[0:2],-ref_line-2.7,'-k')
+        plt.plot()
+        plt.legend(('MUSCL - $L_2$ norm','2nd order'))
+        plt.ylabel('$log_2(Erro_{L_2})$')
+        plt.xlabel('$log_2(N)$')
+        plt.grid()
+        plt.savefig('results/compositional/CILAMCE_L2_norm' + mode +'.png')
+        import pdb; pdb.set_trace()
         datas = np.load('flying/results_water_inj_6k_256_MUSCL_modified_case_LLF_2826.npy', allow_pickle=True)
         for data in datas[2:]:
             Sw_MUSCL256 = data[5]
@@ -160,10 +185,10 @@ for arq in arquivos:
             pressure_MUSCL256 = data[4]/1e3
             n=256
             x256 = np.linspace(0.54624/n,2731.2/n*(n-1),n)
-            e256_max = max(abs(P[0:-1:256]-pressure_MUSCL256))
-            e256_L2 = (sum((P[0:-1:256]-pressure_MUSCL256)**2)*(1/15)**2)**(1/2)
-            R256_L2 = math.log(e128_L2/e256_L2,3)
-            R256_max = math.log(e128_max/e256_max, 3)
+            e256_max = max(abs(Sw_CMG[0:-1:256]-Sw_MUSCL256))
+            e256_L2 = (sum((Sw_CMG[0:-1:256]-Sw_MUSCL256)**2)*(1/256))**(1/2)
+            R256_L2 = math.log(e128_L2/e256_L2,2)
+            R256_max = math.log(e128_max/e256_max, 2)
 
         mode = 'LLF'
 
